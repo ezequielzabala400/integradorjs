6bbd68cff6df80ec0069009c7d8efcd66353556d
ezequielzabala400 <ezequielzabala400@gmail.com>
2023-01-19 23:44:41 -0300

    Fix navbar, change banner, add mediaqueries

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,7 +1,8 @@
 const toggleMenu = document.querySelector('#ToggleMenu');
 const toggleCart = document.querySelector('#ToggleCart');
-const navbar = document.querySelector('#Navbar')
-const Cart = document.querySelector('#Cart')
+const navbar = document.querySelector('#Navbar');
+const Cart = document.querySelector('#Cart');
+const slider = document.querySelector('.Hero__Slider');
 
 const callMenu = () => {
         navbar.classList.toggle('hidden')
@@ -10,5 +11,28 @@ const callMenu = () => {
 const callCart = () => {
     Cart.classList.toggle('hidden')
 }
-toggleCart.addEventListener('click', callCart)
-toggleMenu.addEventListener('click', callMenu)
+
+const heroSlider = () =>{
+let img = ["./assets/img/test/1.jpg", "./assets/img/test/2.jpg", "./assets/img/test/3.jpg", "./assets/img/test/4.jpg"];
+document.HeroSlider.src = img[3]
+
+let i = 0
+const changeImg = () => {
+        document.HeroSlider.src = img[i];
+        if(i < 3){
+            i++
+        }else{
+            i = 0;
+        }
+    }
+    setInterval(changeImg,1600)
+}
+
+
+function init (){
+    window.addEventListener('DOMContentLoaded', heroSlider)
+    toggleCart.addEventListener('click', callCart)
+    toggleMenu.addEventListener('click', callMenu)
+}
+
+init();
